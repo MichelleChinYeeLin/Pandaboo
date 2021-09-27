fd3713a53a40cb371df1f2f82fd6d1bcd1b89b5d
taite <taiengchan@gmail.com>
2021-09-27 15:26:08 +0800

    tt

--- app/src/main/java/com/example/pandaboo/Planner.java
+++ app/src/main/java/com/example/pandaboo/Planner.java
@@ -1,7 +1,7 @@
 package com.example.pandaboo;
 
 public class Planner {
-
+    //private attributes
     private String eventTitle;
     private String eventDetails;
     private int startTime;
@@ -10,8 +10,43 @@ public class Planner {
     private Date endDate;
     private Reminder reminder;
 
+    //getters
     public String getEventTitle(){
 
     }
+    public String getEventDetails(){
 
+    }
+    public int getStartTime(){
+
+    }
+    public int getEndTime() {
+
+    }
+    public Date getDueDate(){
+
+    }
+
+    //setters
+    public void setReminder(Reminder){
+
+    }
+    public void setEventTitle(String){
+
+    }
+    public void setEventDetails(String){
+
+    }
+    public void setStartTime(int){
+
+    }
+    public void setEndTime(int){
+
+    }
+    public void setStartDate(Date){
+
+    }
+    public void setEndDate(Date){
+
+    }
 }
