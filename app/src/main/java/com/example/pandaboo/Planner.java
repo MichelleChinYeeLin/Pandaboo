c0733806ef1631c0eb737188449ef7dac691435e
taite <taiengchan@gmail.com>
2021-09-27 15:29:59 +0800

    tt

--- app/src/main/java/com/example/pandaboo/Planner.java
+++ app/src/main/java/com/example/pandaboo/Planner.java
@@ -5,48 +5,51 @@ public class Planner {
     private String eventTitle;
     private String eventDetails;
     private int startTime;
-    private int startTime;
+    private int endTime;
     private Date startDate;
     private Date endDate;
     private Reminder reminder;
 
     //getters
     public String getEventTitle(){
-
+        this.eventTitle = eventTitle;
     }
     public String getEventDetails(){
-
+        this.eventDetails = eventDetails;
     }
     public int getStartTime(){
-
+        this.startTime = startTime;
     }
     public int getEndTime() {
-
+        this.endTime = endTime;
     }
-    public Date getDueDate(){
-
+    public Date getStartDate(){
+        this.startDate = startDate;
+    }
+    public Date getEndDate(){
+        this.endDate = endDate;
     }
 
     //setters
-    public void setReminder(Reminder){
+    public void setReminder(Reminder reminder){
 
     }
-    public void setEventTitle(String){
+    public void setEventTitle(String eventTitle){
 
     }
-    public void setEventDetails(String){
+    public void setEventDetails(String eventDetails){
 
     }
-    public void setStartTime(int){
+    public void setStartTime(int startTime){
 
     }
-    public void setEndTime(int){
+    public void setEndTime(int endTime){
 
     }
-    public void setStartDate(Date){
+    public void setStartDate(Date startDate){
 
     }
-    public void setEndDate(Date){
+    public void setEndDate(Date endDate){
 
     }
 }
