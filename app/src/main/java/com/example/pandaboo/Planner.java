fa80cc963874f19b1052007e8ca25a11ff35172d
taite <taiengchan@gmail.com>
2021-09-27 15:20:37 +0800

    tt

--- app/src/main/java/com/example/pandaboo/Planner.java
+++ app/src/main/java/com/example/pandaboo/Planner.java
@@ -1,4 +1,17 @@
 package com.example.pandaboo;
 
 public class Planner {
+
+    private String eventTitle;
+    private String eventDetails;
+    private int startTime;
+    private int startTime;
+    private Date startDate;
+    private Date endDate;
+    private Reminder reminder;
+
+    public String getEventTitle(){
+
+    }
+
 }
