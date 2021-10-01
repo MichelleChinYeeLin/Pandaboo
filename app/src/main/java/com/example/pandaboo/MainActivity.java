430c364c56a61082e0cdbca2d59eccd1510b9b17
MichelleChinYeeLin <p20012428@student.newinti.edu.my>
2021-10-01 16:20:02 +0800

    Started on the home page layout (home_main.xml) but the timer does not work (commented out)

--- app/src/main/java/com/example/pandaboo/MainActivity.java
+++ app/src/main/java/com/example/pandaboo/MainActivity.java
@@ -2,18 +2,37 @@ package com.example.pandaboo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
     }
 
-    public void login(View view) {
-
+    /**
+     * Allows the user to switch to different activities
+     * @param view
+     */
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.loginButton:
+                Intent toHome = new Intent(this, HomeMain.class);
+                startActivity(toHome);
+                break;
+            //Add case for register new account
+            //Add case for reset password
+        }
     }
 }
