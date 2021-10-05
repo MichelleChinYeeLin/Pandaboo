b985b8424ae94e45774e18ba0a623dac84e0ed48
taite <taiengchan@gmail.com>
2021-10-05 13:49:54 +0800

    new changes

--- app/src/main/java/com/example/pandaboo/HomeMain.java
+++ app/src/main/java/com/example/pandaboo/HomeMain.java
@@ -1,5 +1,6 @@
 package com.example.pandaboo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -11,6 +12,11 @@ import androidx.appcompat.app.AppCompatActivity;
 public class HomeMain extends AppCompatActivity implements View.OnClickListener {
 
     private Button startButton;
+    private Button pandaButton;
+    private Button shopButton;
+    private Button eventButton;
+    private Button tasksButton;
+    private Button settingsButton;
     private TextView timerCountdown;
     public int counter = 30;
 
@@ -21,6 +27,21 @@ public class HomeMain extends AppCompatActivity implements View.OnClickListener 
 
         startButton = findViewById(R.id.startButton);
         startButton.setOnClickListener(this);
+
+        pandaButton = findViewById(R.id.pandaButton);
+        pandaButton.setOnClickListener(this);
+
+        shopButton = findViewById(R.id.shopButton);
+        shopButton.setOnClickListener(this);
+
+        eventButton = findViewById(R.id.eventButton);
+        eventButton.setOnClickListener(this);
+
+        tasksButton = findViewById(R.id.tasksButton);
+        tasksButton.setOnClickListener(this);
+
+        settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(this);
     }
 
     @Override
@@ -28,6 +49,27 @@ public class HomeMain extends AppCompatActivity implements View.OnClickListener 
         switch(view.getId()){
             case R.id.startButton:
                 startTimer();
+                break;
+            case R.id.pandaButton:
+                Intent toPanda = new Intent(this, Panda.class);
+                startActivity(toPanda);
+                break;
+            case R.id.shopButton:
+                Intent toShop = new Intent(this, Shop.class);
+                startActivity(toShop);
+                break;
+            case R.id.eventButton:
+                Intent toEvent = new Intent(this, Event.class);
+                startActivity(toEvent);
+                break;
+            case R.id.tasksButton:
+                Intent toTasks = new Intent(this, Task.class);
+                startActivity(toTasks);
+                break;
+            case R.id.settingsButton:
+                Intent toSettings = new Intent(this, Settings.class);
+                startActivity(toSettings);
+                break;
         }
     }
 
---
+++ app/src/main/java/com/example/pandaboo/Shop.java
@@ -0,0 +1,20 @@
+package com.example.pandaboo;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Shop extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shop);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
