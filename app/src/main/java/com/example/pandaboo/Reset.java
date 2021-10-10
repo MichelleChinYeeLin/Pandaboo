72ffb60b247a09a22b8f1ba072cd315bc8dd9536
Padmessh <p20012426@student.newinti.edu.my>
2021-10-10 22:44:56 +0800

    *some remidies and Reset.java class

---
+++ app/src/main/java/com/example/pandaboo/Reset.java
@@ -0,0 +1,14 @@
+package com.example.pandaboo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Reset extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset);
+    }
+
+}
