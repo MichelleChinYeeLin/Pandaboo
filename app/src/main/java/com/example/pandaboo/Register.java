5492c1025bf53870848a7e4768eda03e6d39675b
saltaelemon <p20012534@student.newinti.edu.my>
2021-10-13 11:55:26 +0800

    All 3 login XML Final ver (should not have errors)

--- app/src/main/java/com/example/pandaboo/Register.java
+++ app/src/main/java/com/example/pandaboo/Register.java
@@ -3,12 +3,34 @@ package com.example.pandaboo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+        EditText mUsername,mPassword,mEmail;
+        Button mRegisterBtn;
+        Textview mLoginBtn;
+        FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+
+        /** to connect to XML file cannot be done correctly until XML finish
+        mUsername = findViewById(R.id.username);
+
+        mPassword = findViewById(R.id.password);
+        mEmail    = findViewById(R.id.email);
+        mRegisterBtn = findViewById(R.id.register): **/
+
+
+        mPassword = findViewById(R.id.passwordArea);
+
+
+
+
     }
 }
--- app/src/main/java/com/example/pandaboo/User.java
+++ app/src/main/java/com/example/pandaboo/User.java
@@ -1,22 +1,35 @@
 package com.example.pandaboo;
 
-public class User {
-    //Private attributes
-    private String userName;
-    private String userPassword;
-    private int bambooNum;
 
-    //Getters
-    public String getUserName(){return userName;}
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-    //Setters
-    public void setUserName(String userName){this.userName = userName;}
-    public void setUserPassword(String userPassword){this.userPassword = userPassword;}
+import androidx.appcompat.app.AppCompatActivity;
 
-    /*
-    public boolean createAccount(String userName, String userPassword){
-        if ()
-    }*/
+import com.google.firebase.auth.FirebaseAuth;
 
-
+private class Textview {
+    Textview mLoginBtn;
 }
+
+public class Register extends AppCompatActivity {
+    EditText mUsername,mPassword;
+    Button mLoginBtn;
+    FirebaseAuth fAuth;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+
+        //to connect to XML file cannot be done correctly until XML finish
+         mUsername = findViewById(R.id.username);
+         mPassword = findViewById(R.id.passwordArea);
+         mLoginBtn = findViewById(R.id.loginButton);
+
+
+    }
+
