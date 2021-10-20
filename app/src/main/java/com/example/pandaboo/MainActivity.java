9d535a7c1f846c5a7aa2686fd41439baa757515a
saltaelemon <p20012534@student.newinti.edu.my>
2021-10-20 08:34:16 +0800

    Login and Register complete

--- app/src/main/java/com/example/pandaboo/MainActivity.java
+++ app/src/main/java/com/example/pandaboo/MainActivity.java
@@ -1,38 +1,124 @@
 package com.example.pandaboo;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button loginButton;
+    private FirebaseUser user;
+    private final FirebaseAuth auth = FirebaseAuth.getInstance();
+    private EditText emailEditText,passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginButton = findViewById(R.id.loginButton);
+        initializeUI();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //get the current logged in user information
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        // if user already login redirects to home page
+        if (user != null) {
+
+            Intent toHome = new Intent(this, HomeMain.class);
+            startActivity(toHome);
+        }
+    }
+
+    /**
+     * Initialize user interface binding for login button
+     * login email edittext and login password edittext
+     */
+    private void initializeUI(){
+
+        Button loginButton = findViewById(R.id.loginButton);
+
         loginButton.setOnClickListener(this);
+
+        emailEditText = findViewById(R.id.loginEmail);
+        passwordEditText = findViewById(R.id.loginPassword);
+    }
+
+
+    /**
+     * Sign in user with email and password
+     *
+     * pre-condition: email and password must no be empty
+     *
+     * If the task is successful, redirect the user to main page
+     * else the error will display at the edit text
+     *
+     */
+    private void login(){
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        // if the email edit text is empty
+        // display the error, else no error
+        emailEditText.setError((email.isEmpty())? "Please input your email" : null);
+
+        // if the password edit text is empty
+        // display the error, else no error
+        passwordEditText.setError((password.isEmpty())? "Please input your password" : null);
+
+
+        // if both email field and password field is not empty
+        // login user into firebase and redirect to main page
+        if(!email.isEmpty() && !password.isEmpty()){
+
+            //start process of sign in
+            auth.signInWithEmailAndPassword(email,password)
+
+                    .addOnCompleteListener(task -> {
+
+                        //check is login successful
+                        if(task.isSuccessful()){
+                            user = auth.getCurrentUser();
+                            Intent toHome = new Intent(MainActivity.this, HomeMain.class);
+                            startActivity(toHome); // go to main page
+                        }else{
+                            //display error
+                            Toast.makeText(MainActivity.this,
+                                    Objects.requireNonNull(task.getException()).toString(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+        }
     }
 
     /**
      * Allows the user to switch to different activities
-     * @param view
+     * @param view  view a view or button that is triggered with user click
      */
     @Override
     public void onClick(View view){
-        switch(view.getId()){
-            case R.id.loginButton:
-                Intent toHome = new Intent(this, HomeMain.class);
-                startActivity(toHome);
-                break;
-            //Add case for register new account
-            //Add case for reset password
+
+        if(view instanceof Button){
+            if (view.getId() == R.id.loginButton) {
+                login();
+            }else if(view.getId() == R.id.registerUserBtn){
+                // redirect user to register page
+            }
         }
+
     }
 }
--- app/src/main/java/com/example/pandaboo/Register.java
+++ app/src/main/java/com/example/pandaboo/Register.java
@@ -1,36 +1,122 @@
 package com.example.pandaboo;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
-public class Register extends AppCompatActivity {
-        EditText mUsername,mPassword,mEmail;
-        Button mSignupBtn;
-        TextView mLoginBtn;
-        FirebaseAuth fAuth;
+import java.util.Objects;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    EditText mUsername,mPassword,mEmail;
+    Button mSignupBtn;
+    FirebaseAuth fAuth = FirebaseAuth.getInstance();
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
 
-        mUsername = findViewById(R.id.username);
+        //the initializeUI method to set id to the variables
+        initializeUI();
+    }
 
-        mPassword = findViewById(R.id.editPassword);
-        //mEmail    = findViewById(R.id.email);
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //get the current logged in user information
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        // if user already login redirects to home page
+        if (user != null) {
+            Intent toHome = new Intent(this, HomeMain.class);
+            startActivity(toHome);
+        }
+    }
+
+    /**
+     * Initialize UI is for configuration the view of
+     * the app like edit text, buttons, image, etc
+     * and bind with the id set.
+     *
+     */
+    private void initializeUI(){
+
+        mUsername = findViewById(R.id.registerUsername);
+        mPassword = findViewById(R.id.registerPassword);
+        mEmail = findViewById(R.id.registerEmail);
+
         mSignupBtn = findViewById(R.id.signupButton);
 
+    }
 
+    /**
+     * Sign up or Register user into the firebase.
+     * Precondition: the username, email, password must not be empty
+     * After the signup is complete, it redirect the user to the
+     * login page.
+     *
+     */
+    private void signUp(){
 
+        //get all the input from the user
+        String username = mUsername.getText().toString();
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
 
+        //display error if the edit text field is empty
+        mUsername.setError(username.isEmpty()? "Please input your username":null);
+        mPassword.setError(password.isEmpty()? "Please input your password":null);
+        mEmail.setError(email.isEmpty()? "Please input your email":null);
 
+        //if the username, password and email is not empty
+        //register the user by email and password
+        if(!username.isEmpty() && !password.isEmpty() && !email.isEmpty()){
 
+            // register the user with email and password
+            fAuth.createUserWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(task -> {
+                        if(task.isSuccessful()){
+                            user = fAuth.getCurrentUser();
 
+                            UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(username).build();
+                            user.updateProfile(request);
+
+                            //go to login page
+                            Intent toLogin = new Intent(this, MainActivity.class);
+                            startActivity(toLogin);
+                        }else{
+
+                            Toast.makeText(Register.this,
+                                    Objects.requireNonNull(task.getException()).toString(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v instanceof Button){
+            if(v.getId()==R.id.signupButton){
+                signUp();
+            }else if(v.getId()==R.id.clickSignIn){
+                Intent toLogin = new Intent(this, MainActivity.class);
+                startActivity(toLogin);
+            }
+        }
     }
 }
