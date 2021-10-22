c70bfadfd7864705f9a5b1ddd6b5bbee2be66262
Padmessh <p20012426@student.newinti.edu.my>
2021-10-22 10:31:37 +0800

    *current task changes made here

--- app/src/main/java/com/example/pandaboo/Task_Edit.java
+++ app/src/main/java/com/example/pandaboo/Task_Edit.java
@@ -1,18 +1,23 @@
 package com.example.pandaboo;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,7 +25,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.w3c.dom.Text;
+
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -30,6 +38,12 @@ public class Task_Edit extends AppCompatActivity {
     private View TaskBackButton;
     private AlertDialog.Builder spinnerLangauges;
     final String firebaseURL = "https://pandaboodcs-default-rtdb.asia-southeast1.firebasedatabase.app";
+    Button selectDate;
+    TextView Date;
+    TextView displaySpinner;
+    DatePickerDialog datePickerDialog;
+    int year,month, dayOfMonth;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,19 +59,53 @@ public class Task_Edit extends AppCompatActivity {
             ImageButton taskAddButton = (ImageButton) findViewById(R.id.taskAddButton);
         });
 
+        selectDate = findViewById(R.id.dueDateButton);
+        Date = findViewById(R.id.selectdDate);
+
+        selectDate.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(Task_Edit.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                Date.setText(day + "/" + (month+1) + "/" + year);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.show();
+            }
+        });
+
         //dropdown menu related
         Spinner spinner = (Spinner) findViewById(R.id.dropDownSpinner);
+        //String size = spinner.getSelectedItem().toString();
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinner.setAdapter(adapter);
-    }
+
+        /*spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //update text view
+                displaySpinner.setText(spinner.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            } */
+         }
 
     void saveTodo() {
         // first section
         // get the data to save in our firebase db
-        EditText mainTask = (EditText) findViewById(R.id.taskTitle);
+        EditText mainTask = (EditText) findViewById(R.id.taskTitleEdit);
         EditText subTask = (EditText) findViewById(R.id.subTaskTextEdit);
-        EditText dueDate = (EditText) findViewById(R.id.dueDate);
+        Button dueDate = (Button) findViewById(R.id.dueDateButton);
 
 
         /* //can use datepicker for the date things, need to change dueDate to datepicker in xml
